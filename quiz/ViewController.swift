effa5f8964dc957748f5e4952697c54fd0c05ed0
Yoshua Elmaryono <yoshuaelmaryono@yoshua-e.local>
2018-07-01 12:08:04 +0700

    finish quiz app

--- quiz/ViewController.swift
+++ quiz/ViewController.swift
@@ -13,13 +13,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        questionLabel.text = quizzes[currentQuizIndex].question
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Outlets
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var answerLabel: UILabel!
+
+    //MARK: Actions
+    @IBAction func showAnswer(_ sender: UIButton) {
+        answerLabel.text = quizzes[currentQuizIndex].answer
     }
-
-
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuizIndex += 1
+        currentQuizIndex %= quizzes.count
+        answerLabel.text = "???"
+        questionLabel.text = quizzes[currentQuizIndex].question
+    }
+    
+    //MARK: Model
+    typealias Question = String
+    typealias Answer = String
+    typealias Quiz = (question: Question, answer: Answer)
+    typealias Quizzes = [Quiz]
+    
+    let quizzes: Quizzes = [
+        ("Water?","H20"),
+        ("Oxygen?","02"),
+        ("Ozone?", "O3"),
+    ]
+    var currentQuizIndex = 0
 }
 
